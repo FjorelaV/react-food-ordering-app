267f03dc9ad04da31791d79a32c40b4f9de75443
Fjorela Veliu <fjorelaveliu1995@gmail.com>
2022-12-21 10:49:32 +0100

    feat(Meals): add meals structure

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = (props) => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
