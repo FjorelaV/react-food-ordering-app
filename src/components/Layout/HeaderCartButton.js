09e59543c6d57121f3545b7af0840c037c9bc9f7
Fjorela Veliu <fjorelaveliu1995@gmail.com>
2022-12-22 11:49:07 +0100

    feat(Header): using the context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+
 import classes from "./HeaderCartButton.module.css";
 import CardIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>2</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
