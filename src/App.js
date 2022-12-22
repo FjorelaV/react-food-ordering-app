88e76bfd708405f6e98a861053a9faec4dcec169
Fjorela Veliu <fjorelaveliu1995@gmail.com>
2022-12-22 11:15:40 +0100

    feat: add context first approach

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
-
-  const setCartHandler = () => {
-    setCartIsShown(true);
-  };
 
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -19,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
